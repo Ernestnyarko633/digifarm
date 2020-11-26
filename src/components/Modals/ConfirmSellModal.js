8b25fd24ca795003b3da356c6625af59819351a4
Georgina Adzorgenu <gadzorgenu@gmail.com>
2020-11-26 14:36:53 +0100

    MOdified ConfirmSale modal

--- src/components/Modals/ConfirmSellModal.js
+++ src/components/Modals/ConfirmSellModal.js
@@ -12,7 +12,8 @@ import {
   Box,
   Text,
   Heading,
-  Flex
+  Flex,
+  Divider
 } from '@chakra-ui/core';
 
 const ConfirmSell = () => {
@@ -21,28 +22,33 @@ const ConfirmSell = () => {
   return (
     <>
     <Button mt={4} colorScheme='linear' rounded='30px' fontSize='xs' w={{md: '150px'}} onClick={onOpen}>Sell to buyer</Button>
-   <Modal isOpen={isOpen} onClose={onClose} mt={10} isCentered>
+   <Modal isOpen={isOpen} onClose={onClose} mt={10} variant='outline' borderColor='black' colorScheme='white' isCentered >
         <ModalOverlay />
-        <ModalContent>
-          <ModalCloseButton />
+        <ModalContent >
+          <Flex mt={2}>
+            <Box ml={10} >
+                <Heading as="h4" fontWeight="bold" fontSize={{ md: 'xl' }}>
+                  Confirm Sale
+                </Heading>
+              </Box>
+            <ModalCloseButton />
+          </Flex>
+          <Divider orientation='horizontal'/>
           <ModalBody>
-            <Box textAlign="center" mt={2} py={2}>
-              <Heading as="h4" fontWeight="bold" fontSize={{ md: 'xl' }}>
-                Confirm Sale
-              </Heading>
-            </Box>
-            <Box mt={2} p={2} bg='cf.300' fontFamily='light' textAlign='center'>
+            
+            <Box mt={2} p={2}  fontFamily='light' textAlign='justify' >
             <Text fontSize={{ md: 'sm' }}>
                 You're about to sell 
                 <Text as='span' fontWeight='bold'> 2000 tonnes </Text>  
                 of your produce worth 
                 <Text as ='span' fontWeight='bold'> $540.00 </Text> 
                 to <Text as='span' fontWeight='bold' >John Clinton</Text>.
+                <Text as='span' fontSize={{md: 'sm'}} pl={1}>You  would have <Text as ='span' fontWeight='bold'>500 tonnes</Text> left to sell.
               </Text>
-              <Text fontSize={{md: 'sm'}}>You have <Text as ='span' fontWeight='bold'>500 tonnes</Text> left to sell.
               </Text>
+              
             </Box>
-            <Text fontSize={{md:'xs'}} pl={3} pt={1} fontFamily='light' color='red.500'>Are you sure you want to sell this crop? This action is irreversible</Text>
+            <Text  fontSize={{md:'sm'}} pl={2}  fontWeight='thin' color='red.600'>Are you sure you want to sell this crop? This action is irreversible </Text>
           </ModalBody>
           <ModalFooter>
             <Flex>
