1e924268ca6beaa6d871513730309a6e7b1afb79
DziiOG <lildzii.wd@gmail.com>
2020-11-30 16:27:38 +0000

    update farm-board card

--- src/components/Cards/FarmCard.js
+++ src/components/Cards/FarmCard.js
@@ -1,85 +1,161 @@
-import React from 'react'
-import { Box, Button,Divider, Heading, Flex,ListItem, List, Avatar, Text, Image } from '@chakra-ui/core'
-import PropTypes from 'prop-types'
+import React from "react";
+import {
+  Box,
+  Button,
+  Divider,
+  Heading,
+  Flex,
+  ListItem,
+  List,
+  Avatar,
+  Text,
+  Image,
+  Icon,
+} from "@chakra-ui/core";
+import { TimeIcon, CheckCircleIcon } from "@chakra-ui/icons";
+import { BsCircle, BsQuestionCircleFill } from "react-icons/bs";
+import PropTypes from "prop-types";
 
-const FarmCard = ({ 
-    name,
-    location, 
-    image, 
-    weight, 
-    bags, 
-    quantity, 
-    condition,
-    mr,
-    ml
+const FarmCard = ({
+  name,
+  location,
+  image,
+  weight,
+  bags,
+  quantity,
+  condition,
+  mr,
+  ml,
+  w,
+  h,
+  ...rest
 }) => {
-    return(
-        <Box >
-        <Flex justify='center'> 
-            <Box rounded='lg' bg='white' mr={mr}  ml={ml} p={6}>
-            <Flex mb={4}>
-                <Avatar bg='gray.100'>
-                    <Image src={image} />
-                </Avatar>
-                <Box ml={2}>
-                    <Heading as='h6' fontSize={{md: 'lg'}}>{name}</Heading>
-                    <Text fontSize='xs' mt={{md:-2}}>{location}</Text>
-                </Box>
+  return (
+    <Box>
+      <Flex justify="center">
+        <Box
+          {...rest}
+          boxShadow="0px 0px 15px 5px #ccc"
+          rounded={30}
+          bg="white"
+          w={w || "100%"}
+          h={h || "100%"}
+          mr={mr}
+          ml={ml}
+          p={8}
+        >
+          <Flex mb={4} direction="row" align="center">
+            <Avatar bg="gray.100" mr={5}>
+              <Image src={image} />
+            </Avatar>
+            <Box ml={2}>
+              <Heading as="h6" fontSize={25}>
+                {name || "Task of the day"}
+              </Heading>
+              <Text fontSize="xs" color="#9b9b9b" mt={{ md: -2 }}>
+                {location || "Finish all tasks to earn rewards"}
+              </Text>
+            </Box>
+          </Flex>
+          <Divider borderColor="gray.300" />
+          <Box>
+            <Flex w="100%">
+              <Box ml={2} w="100%">
+                <Flex direction="row" align="center" justify="space-between">
+                  <Flex direction="row" align="center">
+                    <TimeIcon mr={2} />
+                    <Heading as="h6" fontSize={{ md: "md" }}>
+                      {name || "TASK 1"}
+                    </Heading>
+                  </Flex>
+                  <Box>
+                    <CheckCircleIcon color="cf.400" />
+                  </Box>
+                </Flex>
+                <Text fontSize="xs" color="#9b9b9b" mt={{ md: -2 }}>
+                  {location ||
+                    "Update your bank details with the correct information"}
+                </Text>
+              </Box>
             </Flex>
             <Divider borderColor="gray.300" />
-            <Box>
-                <Flex>
-                    <List my={3}>
-                        <ListItem fontSize='xs'>
-                        Volume - <span fontSize='sm' pl={2}>{quantity}</span>
-                        </ListItem>
-                        <ListItem fontSize='xs' >
-                        Weight - <span fontSize='sm' pl={2}>{weight}</span>
-                        </ListItem>
-                        <ListItem fontSize='xs'>
-                        Number of Bags -<span fontSize='sm' pl={2}> {bags}</span>
-                        </ListItem>
-
-                        //TODO: truncate words
-                        <ListItem fontSize='xs'>
-                        Yeild conditions -<span fontSize='sm' pl={2}>{condition}</span>
-                        </ListItem>
-                    </List>
-                <Box ml={6} my={3}>
-                    <Box  rounded='40px' bg='cf.200' my={1} pt={1} px={2}>
-                        <Text color='cf.400' fontSize='9px' textAlign='center'>
-                            Pending Order
-                        </Text>
-                    </Box>
-                    <Text as='h6' fontSize='9px' ml={2} fontWeight='bold'>80% Complete</Text>
-                </Box>
+          </Box>
+          <Box>
+            <Flex>
+              <Box ml={2}>
+                <Flex direction="row" align="center" justify="space-between">
+                  <Flex direction="row" align="center">
+                    <TimeIcon mr={2} />
+                    <Heading as="h6" fontSize={{ md: "md" }}>
+                      {name || "TASK 2"}
+                    </Heading>
+                  </Flex>
+                  <Box>
+                    <Icon as={BsCircle} color="cf.400" />
+                  </Box>
                 </Flex>
-            </Box>
-            <Button
-            colorScheme='linear'
-            rounded='30px'
-            fontSize='xs'
-            fontWeight='bold'
-            width="100%"
-            >
-            Sell
-            </Button>
+                <Text fontSize="xs" mt={{ md: -2 }} color="#9b9b9b">
+                  {location ||
+                    "Update your bank details with the correct information"}
+                </Text>
+              </Box>
+            </Flex>
+            <Divider borderColor="gray.300" />
+          </Box>
+          <Box>
+            <Flex>
+              <Box ml={2}>
+                <Flex direction="row" align="center" justify="space-between">
+                  <Flex direction="row" align="center">
+                    <TimeIcon mr={2} />
+                    <Heading as="h6" fontSize={{ md: "md" }}>
+                      {name || "TASK 3"}
+                    </Heading>
+                  </Flex>
+                  <Box>
+                    <Icon as={BsCircle} color="cf.400" />
+                  </Box>
+                </Flex>
+                <Text fontSize="xs" mt={{ md: -2 }} color="#9b9b9b">
+                  {location ||
+                    "Update your bank details with the correct information"}
+                </Text>
+              </Box>
+            </Flex>
+            <Divider borderColor="gray.300" />
+            <Flex ml={2} py={3}>
+              <Box>
+                <Flex direction="row" >
+                  <Icon mr={2} mt={1} as={BsQuestionCircleFill} color="cf.400"/>
+                  <Flex  direction="column" >
+                    <Text fontSize={9} color="cf.400">
+                      {location || "Reward: 2 points"}
+                    </Text>
+                    <Text fontSize={7} color="#9b9b9b">
+                      {location || "get 20 points to earn a badge"}
+                      <br></br>
+                      {"get 20 points to earn a badge"}
+                    </Text>
+                  </Flex>
+                </Flex>
+              </Box>
+            </Flex>
+          </Box>
         </Box>
-        </Flex>
-        </Box>
-    )
-}
+      </Flex>
+    </Box>
+  );
+};
 
 FarmCard.propTypes = {
-    name: PropTypes.string.isRequired,
-    location:PropTypes.string.isRequired,
-    image: PropTypes.any.isRequired,
-    buttontitle: PropTypes.string.isRequired,
-    weight: PropTypes.string.isRequired,
-    bags: PropTypes.string.isRequired,
-    quantity: PropTypes.string.isRequired,
-    condition: PropTypes.string.isRequired,
-    mr: PropTypes.any
-
-}
-export default FarmCard
+  name: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  image: PropTypes.any.isRequired,
+  buttontitle: PropTypes.string.isRequired,
+  weight: PropTypes.string.isRequired,
+  bags: PropTypes.string.isRequired,
+  quantity: PropTypes.string.isRequired,
+  condition: PropTypes.string.isRequired,
+  mr: PropTypes.any,
+};
+export default FarmCard;
--- src/components/EmptyStates/FarmsEmptyState.js
+++ src/components/EmptyStates/FarmsEmptyState.js
@@ -10,10 +10,12 @@ import {
   Link,
   Text,
 } from "@chakra-ui/core";
+
 import CropCard from "components/Cards/CropCard";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import CorporateIllustration from "../../assets/images/emptystate/corporate.png";
 import PotatoImg from "../../assets/images/emptystate/potato.png";
+import FarmCard from "../Cards/FarmCard";
 
 const FarmsEmptyState = () => {
   const availableFarms = [
@@ -48,39 +50,15 @@ const FarmsEmptyState = () => {
   ];
 
   return (
-    <Box>
-      <Box bg="rgba(155, 155, 155, 0.1)" p={{ md: 10 }} mt={{ md: 24 }}>
-        {/* <Box textAlign="center">
-        <Heading as="h3" fontSize={{ md: '3xl' }} mt={{ md: 6 }}>
-          We are connecting the world through agriculture
-        </Heading>
-        <Text>
-          Be part of a cooperative where you can connect to people around the
-          <br />
-          world and co-own a farm in Africa.
-        </Text>
-      </Box> */}
-
-        {/* <Box my={{ md: 10 }} textAlign="center">
-        <Link _hover={{ textDecor: 'none' }} color="cf.400" d="block" mb={4}>
-          Learn more <Icon as={MdKeyboardArrowRight} />
-        </Link>
-
-        <Button colorScheme="linear" rounded="30px" w={64} h={12} shadow="md">
-          Get me started now
-        </Button>
-      </Box> */}
-
-        {/* <Box mx="auto" w="80%" my={{ md: 16 }}>
-        <Image src={CorporateIllustration} />
-      </Box> */}
-        <Box p={{ md: 16 }}>
+    <Box bg="rgba(155, 155, 155, 0.1)">
+      <Box p={{ md: 8 }} mt={{ md: 20 }}>
+        <Box> 
           <Flex direction="row" w="100%">
-            <Box w="50%" bg="black">
+            <Box w="50%" >
               <Text>Hello</Text>
             </Box>
-            <Box w="50%" bg="#00ff00">
-              <Text>Hello</Text>
+            <Box w="50%">
+              <FarmCard w="60%" h="380px"/>
             </Box>
           </Flex>
         </Box>
