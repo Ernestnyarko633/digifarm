b17cb8bc1492113223a80bea78dcb4527df05a26
DziiOG <lildzii.wd@gmail.com>
2020-12-21 11:58:58 +0000

    updated sharing function and scroll to reveal

--- src/components/Cards/FarmBoardCard.js
+++ src/components/Cards/FarmBoardCard.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
   Box,
   Flex,
   Divider,
@@ -11,11 +17,16 @@ import {
   Grid
 } from '@chakra-ui/react'
 import PropTypes from 'prop-types'
-
 import { MdFavoriteBorder } from 'react-icons/md'
+import { AiFillTwitterCircle, AiFillFacebook, AiFillLinkedin } from 'react-icons/ai'
 import { RiShareForwardLine } from 'react-icons/ri'
 import Button from 'components/Button'
 import { Fragment } from 'react'
+import {
+  TwitterShareButton,
+  LinkedinShareButton,
+  FacebookShareButton
+} from 'react-share'
 
 const FarmBoardCard = ({
   w,
@@ -47,8 +58,13 @@ const FarmBoardCard = ({
   AvatarSRC,
   ...rest
 }) => {
+
+ 
+  const { isOpen, onClose, onOpen } = useDisclosure()
+  const shareUrl = 'http://completefarmer.com'
+
   return (
-    <Box w={{ md: '100%' }}>
+    <Box w={{ md: '100%' }} zIndex={10}>
       <Flex justify='center' align='center'>
         <Box rounded='lg'
           bg='white'
@@ -167,11 +183,46 @@ const FarmBoardCard = ({
                     pl={10}>
                     <Flex align='center' justify='center' mr={2}>
                       <Icon boxSize={8} as={MdFavoriteBorder} mr={2} />
-                      <Text fontSize='xs' color='gray.400'>
+                      <Text fontSize='xs' color='gray.500'>
                         {likeCount}
                       </Text>
                     </Flex>
-                    <Icon boxSize={8} as={RiShareForwardLine} />
+                    {/*  */}
+                    <Icon boxSize={8} as={RiShareForwardLine} onClick={onOpen} />  
+                    <Modal isOpen={isOpen}
+        onClose={onClose}
+        mt={10}
+        variant='outline'
+        borderColor='black'
+        borderWidth={5}
+        colorScheme='white'
+        isCentered>
+                      <ModalOverlay />
+                      <ModalContent>
+                        <Flex mt={2} mb={3}>
+                          <Box ml={10}>
+                            <Heading as='h4' fontWeight='bold' fontSize={{ md: 'xl' }}>
+                              Share
+                            </Heading>
+                          </Box>
+                          <ModalCloseButton />
+                        </Flex>
+                        <Divider orientation='horizontal' borderColor='gray.400' />
+                        <ModalBody>
+                          <Flex direction='row' justify='center' align='center'>
+                            <Box as={TwitterShareButton} boxSize={50} title={postContentText} url={shareUrl} via={postContentText}>
+                              <Icon boxSize={8} as={AiFillTwitterCircle} />
+                            </Box>
+                            <Box as={LinkedinShareButton} boxSize={50} title={postContentText} url={shareUrl} via={postContentText}>
+                              <Icon boxSize={8} as={AiFillLinkedin} />
+                            </Box>
+                            <Box as={FacebookShareButton} boxSize={50} title={postContentText} url={shareUrl} via={postContentText}>
+                              <Icon boxSize={8} as={AiFillFacebook} />
+                            </Box>
+                          </Flex>
+                        </ModalBody>
+                      </ModalContent>
+                    </Modal> 
                   </Flex>
                 </Box>
               </Box>
