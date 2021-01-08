7d2febb8c3d71d0fd031bde571afef0803591a72
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-01-08 12:17:45 +0100

    Fixes

--- src/pages/FarmBoard.js
+++ src/pages/FarmBoard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from 'container/Layout'
 import FarmsEmptyState from 'components/EmptyStates/FarmsEmptyState'
 import Greetings from 'components/Utils/FarmBoardGreetings'
-import FarmBoardContent from 'components/FarmBoard/FarmBoard'
+import FarmBoardContent from 'components/FarmBoard/FarmBoardContent'
 
 const FarmBoard = () => {
   document.title = 'Complete Farmer | Farmboard'
