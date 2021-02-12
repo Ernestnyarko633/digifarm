d5225b492b67aa36d54cbe7af335ed5cc2d05390
Felix Nana Yaw Yeboah - Jefferson <gabsco208309@hotmail.com>
2021-02-12 14:30:17 +0000

    Farm cards

---
+++ src/components/Dynamic/Cards/WeatherCards.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Cloud } from 'theme/Icons'
+import { Box, Grid, Heading, Icon, Text } from '@chakra-ui/react'
+
+export default function WeatherCard() {
+  return (
+    <Grid templateColumns={{ md: 'repeat(2, 1fr)' }} gap={8} my={{ md: 8 }}>
+      <Box
+        w='100%'
+        rounded='lg'
+        filter='drop-shadow(0px 2px 20px rgba(0, 0, 0, 0.1))'
+        p={6}
+        bg='white'
+      >
+        <Text textAlign='center' fontWeight={300}>
+          Plant population
+        </Text>
+      </Box>
+      <Box
+        w='100%'
+        rounded='lg'
+        filter='drop-shadow(0px 2px 20px rgba(0, 0, 0, 0.1))'
+        p={6}
+        bg='white'
+      >
+        <Text fontWeight={300}>Weather today</Text>
+        <Box mt={2}>
+          <Icon as={Cloud} boxSize={10} />
+          <Heading fontSize={{ md: '6xl' }} fontWeight={900} mt={1}>
+            11C
+          </Heading>
+        </Box>
+      </Box>
+    </Grid>
+  )
+}
