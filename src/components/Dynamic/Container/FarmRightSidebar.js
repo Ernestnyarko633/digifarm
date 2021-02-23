37dff107116c0cf60acbcf70df4ce8a7d3f76b4e
Felix Nana Yaw Yeboah - Jefferson <gabsco208309@hotmail.com>
2021-02-23 13:29:05 +0000

    fixed bugs and refactored code

--- src/components/Dynamic/Container/FarmRightSidebar.js
+++ src/components/Dynamic/Container/FarmRightSidebar.js
@@ -1,37 +1,36 @@
-import { Box, Spinner, Text } from '@chakra-ui/react'
+import { Box, Skeleton, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
 import PropTypes from 'prop-types'
 import DynamicCard from '../Sidebar'
 import useApi from 'context/api'
 
-export default function FarmRightSidebar({ state, digitalFarmerFarm }) {
+export default function FarmRightSidebar({ state, farms }) {
   const [scheduledTasks, setScheduledTasks] = React.useState([])
   const [farmfeeds, setFarmFeeds] = React.useState([])
-  const [loading, setLoading] = React.useState('fetching')
+  const [loading, setLoading] = React.useState(false)
   const [error, setError] = React.useState(null)
   const { getMyScheduledTasks, getMyFarmFeeds } = useApi()
 
   React.useEffect(() => {
     const fetchData = async () => {
       try {
-        setLoading('fetching')
-        const res = await getMyScheduledTasks({ farm: digitalFarmerFarm })
+        setLoading(true)
+        const res = await getMyScheduledTasks({ farm: farms })
         setScheduledTasks(res.data)
 
-        setLoading('done')
+        setLoading(false)
       } catch (error) {
-        setLoading('done')
         setError(error)
       }
     }
     fetchData()
-  }, [digitalFarmerFarm, getMyScheduledTasks])
+  }, [farms, getMyScheduledTasks])
 
   React.useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading('fetching')
-        const res = await getMyFarmFeeds({ farm: digitalFarmerFarm })
+        const res = await getMyFarmFeeds({ farm: farms })
         setFarmFeeds(res.data)
 
         setLoading('done')
@@ -41,7 +40,29 @@ export default function FarmRightSidebar({ state, digitalFarmerFarm }) {
       }
     }
     fetchData()
-  }, [digitalFarmerFarm, getMyFarmFeeds])
+  }, [farms, getMyFarmFeeds])
+
+  if (loading) {
+    return (
+      <Stack p={10} mt={24} spacing={4}>
+        <Skeleton bg='gray.100' height='200px' rounded='lg' />
+        <Skeleton bg='gray.100' height='200px' rounded='lg' />
+        <Skeleton bg='gray.100' height='200px' rounded='lg' />
+        <Skeleton bg='gray.100' height='200px' rounded='lg' />
+      </Stack>
+    )
+  }
+
+  if (error) {
+    return (
+      <Box>
+        <Text fontSize='md' ml={2} color='cf.400'>
+          Something went wrong
+        </Text>
+      </Box>
+    )
+  }
+
   return (
     <Box
       py={8}
@@ -56,28 +77,18 @@ export default function FarmRightSidebar({ state, digitalFarmerFarm }) {
       shadow='md'
       overflowY='scroll'
     >
-      {loading === 'fetching' && <Spinner size='lg' color='cf.400' />}
-      {loading === 'done' && (
-        <DynamicCard
-          card={state}
-          scheduledTasks={scheduledTasks}
-          farmfeeds={farmfeeds}
-          farm={digitalFarmerFarm}
-        />
-      )}
-      {loading === 'done' && error && (
-        <Box>
-          <Text fontSize='md' ml={2} color='cf.400'>
-            Something went wrong
-          </Text>
-        </Box>
-      )}
+      <DynamicCard
+        card={state}
+        scheduledTasks={scheduledTasks}
+        farmfeeds={farmfeeds}
+        farms={farms}
+      />
     </Box>
   )
 }
 
 FarmRightSidebar.propTypes = {
   state: PropTypes.string,
-  digitalFarmerFarm: PropTypes.any,
+  farms: PropTypes.any,
   farmfeeds: PropTypes.any
 }
