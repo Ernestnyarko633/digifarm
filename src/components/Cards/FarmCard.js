be4e007f4d558ff2a02ce03b57aecc2b20d5a64e
DziiOG <lildzii.wd@gmail.com>
2021-03-27 06:02:52 +0000

    farm wallet update

--- src/components/Cards/FarmCard.js
+++ src/components/Cards/FarmCard.js
@@ -16,101 +16,72 @@ import PropTypes from 'prop-types'
 const FarmCard = ({ farm, _small }) => {
   return (
     <React.Fragment>
-      {!_small ? (
-        <Box
-          rounded='xl'
-          shadow='md'
-          p={10}
-          bg='white'
-          minW={{ md: 130 }}
-          mr={{ md: 6 }}
-        >
-          <Flex align='center' justify='space-between'>
-            <Flex align='center'>
-              <Box mr={4}>
-                <Avatar src={require('../../assets/images/soya.png').default} />
-              </Box>
+      <Box
+        rounded='xl'
+        shadow='md'
+        p={10}
+        bg='white'
+        minW={{ md: _small ? 85 : 130 }}
+        mr={{ md: 6 }}
+      >
+        <Flex align='center' justify='space-between'>
+          <Flex align='center'>
+            <Box mr={4}>
+              <Avatar src={require('../../assets/images/soya.png').default} />
+            </Box>
 
-              <Box>
-                <Heading as='h4' fontSize={{ md: '2xl' }}>
-                  {farm.name}
-                </Heading>
-                <Text color='gray.500' mt={-1}>
-                  {farm.location}
-                </Text>
-              </Box>
-            </Flex>
-
-            <Tag
-              bg='cf.200'
-              color='cf.400'
-              rounded='3xl'
-              fontSize='sm'
-              px={4}
-              textAlign='center'
-            >
-              Lvl {farm.level}
-            </Tag>
-          </Flex>
-          <Divider orientation='horizontal' borderColor='gray.300' my={6} />
-
-          <Grid templateColumns={{ md: 'repeat(2, 1fr)' }} gap={16}>
             <Box>
               <Heading as='h4' fontSize={{ md: '2xl' }}>
-                Progress on farm
+                {farm.name}
               </Heading>
-              <Divider orientation='horizontal' borderColor='gray.300' my={3} />
-              <Box>
-                <Step />
-                <Step />
-                <Step />
-                <Step />
-                <Step />
-              </Box>
+              <Text color='gray.500' mt={-1}>
+                {farm.location}
+              </Text>
             </Box>
-            <Box>
-              <Image src={require('../../assets/images/farmimg.png').default} />
-            </Box>
-          </Grid>
-        </Box>
-      ) : (
-        <Box
-          rounded='xl'
-          shadow='md'
-          p={10}
-          bg='white'
-          minW={{ md: 85 }}
-          mr={{ md: 6 }}
-        >
-          <Flex align='center' justify='space-between'>
-            <Flex align='center'>
-              <Box mr={4}>
-                <Avatar src={require('../../assets/images/soya.png').default} />
-              </Box>
+          </Flex>
 
+          <Tag
+            bg='cf.200'
+            color='cf.400'
+            rounded='3xl'
+            fontSize='sm'
+            px={4}
+            textAlign='center'
+          >
+            Lvl {farm.level}
+          </Tag>
+        </Flex>
+        {!_small && (
+          <React.Fragment>
+            <Divider orientation='horizontal' borderColor='gray.300' my={6} />
+
+            <Grid templateColumns={{ md: 'repeat(2, 1fr)' }} gap={16}>
               <Box>
                 <Heading as='h4' fontSize={{ md: '2xl' }}>
-                  {farm.name}
+                  Progress on farm
                 </Heading>
-                <Text color='gray.500' mt={-1}>
-                  {farm.location}
-                </Text>
+                <Divider
+                  orientation='horizontal'
+                  borderColor='gray.300'
+                  my={3}
+                />
+                <Box>
+                  <Step />
+                  <Step />
+                  <Step />
+                  <Step />
+                  <Step />
+                </Box>
               </Box>
-            </Flex>
-
-            <Tag
-              bg='cf.200'
-              color='cf.400'
-              rounded='3xl'
-              fontSize='sm'
-              px={4}
-              textAlign='center'
-            >
-              Lvl {farm.level}
-            </Tag>
-          </Flex>
-        </Box>
-      )}
+              <Box>
+                <Image
+                  src={require('../../assets/images/farmimg.png').default}
+                />
+              </Box>
+            </Grid>
+          </React.Fragment>
+        )}
+      </Box>
     </React.Fragment>
   )
 }
