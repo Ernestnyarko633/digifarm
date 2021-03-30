650b85793396ed524e1ec535da22a5dbce55614d
Felix Nana Yaw Yeboah - Jefferson <felix@hubtel.com>
2021-03-30 22:00:47 +0000

    updated profile page

--- src/components/Cards/FarmCard.js
+++ src/components/Cards/FarmCard.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import {
   Avatar,
   Box,
@@ -6,73 +7,88 @@ import {
   Grid,
   Heading,
   Image,
+  Link,
   Tag,
-  Text
-} from '@chakra-ui/react'
-import Step from 'components/Form/Step'
-import React from 'react'
-import PropTypes from 'prop-types'
+  Text,
+} from '@chakra-ui/react';
+import { Link as ReachRouter } from 'react-router-dom';
+import Step from 'components/Form/Step';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const FarmCard = ({ farm, _small }) => {
+  console.log('farm', farm);
   return (
-    <React.Fragment>
+    <>
       {!_small ? (
-        <Box
-          rounded='xl'
-          shadow='md'
-          p={10}
-          bg='white'
-          minW={{ md: 130 }}
-          mr={{ md: 6 }}
-        >
-          <Flex align='center' justify='space-between'>
-            <Flex align='center'>
-              <Box mr={4}>
-                <Avatar src={require('../../assets/images/soya.png').default} />
-              </Box>
-
-              <Box>
-                <Heading as='h4' fontSize={{ md: '2xl' }}>
-                  {farm.name}
-                </Heading>
-                <Text color='gray.500' mt={-1}>
-                  {farm.location}
-                </Text>
-              </Box>
-            </Flex>
-
-            <Tag
-              bg='cf.200'
-              color='cf.400'
-              rounded='3xl'
-              fontSize='sm'
-              px={4}
-              textAlign='center'
+        <ReachRouter to={`/farms/${farm?._id}`}>
+          <Link _hover={{ textDecor: 'none' }}>
+            <Box
+              rounded='xl'
+              shadow='md'
+              p={10}
+              bg='white'
+              minW={{ md: 130 }}
+              mr={{ md: 6 }}
             >
-              Lvl {farm.level}
-            </Tag>
-          </Flex>
-          <Divider orientation='horizontal' borderColor='gray.300' my={6} />
+              <Flex align='center' justify='space-between'>
+                <Flex align='center'>
+                  <Box mr={4}>
+                    <Avatar
+                      src={require('../../assets/images/soya.png').default}
+                    />
+                  </Box>
 
-          <Grid templateColumns={{ md: 'repeat(2, 1fr)' }} gap={16}>
-            <Box>
-              <Heading as='h4' fontSize={{ md: '2xl' }}>
-                Progress on farm
-              </Heading>
-              <Divider orientation='horizontal' borderColor='gray.300' my={3} />
-              <Box>
-                <Step />
-                <Step />
-                <Step />
-                <Step />
-                <Step />
-              </Box>
+                  <Box>
+                    <Heading as='h4' fontSize={{ md: '2xl' }}>
+                      {farm.name}
+                    </Heading>
+                    <Text color='gray.500' mt={-1}>
+                      {farm.location}
+                    </Text>
+                  </Box>
+                </Flex>
+
+                <Tag
+                  bg='cf.200'
+                  color='cf.400'
+                  rounded='3xl'
+                  fontSize='sm'
+                  px={4}
+                  textAlign='center'
+                >
+                  Lvl {farm.level}
+                </Tag>
+              </Flex>
+              <Divider orientation='horizontal' borderColor='gray.300' my={6} />
+
+              <Grid templateColumns={{ md: 'repeat(2, 1fr)' }} gap={16}>
+                <Box>
+                  <Heading as='h4' fontSize={{ md: '2xl' }}>
+                    Progress on farm
+                  </Heading>
+                  <Divider
+                    orientation='horizontal'
+                    borderColor='gray.300'
+                    my={3}
+                  />
+                  <Box>
+                    <Step />
+                    <Step />
+                    <Step />
+                    <Step />
+                    <Step />
+                  </Box>
+                </Box>
+                <Box>
+                  <Image
+                    src={require('../../assets/images/farmimg.png').default}
+                  />
+                </Box>
+              </Grid>
             </Box>
-            <Box>
-              <Image src={require('../../assets/images/farmimg.png').default} />
-            </Box>
-          </Grid>
-        </Box>
+          </Link>
+        </ReachRouter>
       ) : (
         <Box
           rounded='xl'
@@ -111,17 +127,17 @@ const FarmCard = ({ farm, _small }) => {
           </Flex>
         </Box>
       )}
-    </React.Fragment>
-  )
-}
+    </>
+  );
+};
 
 FarmCard.propTypes = {
   farm: PropTypes.shape({
     name: PropTypes.string,
     location: PropTypes.string,
-    level: PropTypes.number
+    level: PropTypes.number,
   }),
-  _small: PropTypes.bool
-}
+  _small: PropTypes.bool,
+};
 
-export default FarmCard
+export default FarmCard;
