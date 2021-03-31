e6336ec34bf4ba50908f5a76e6fb2471a5dbb722
DziiOG <lildzii.wd@gmail.com>
2021-03-31 09:47:46 +0000

    update*

--- src/components/Dashboard/FarmOrderSection.js
+++ src/components/Dashboard/FarmOrderSection.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Box, Flex, Grid, Heading } from '@chakra-ui/react'
-
-import ArrowButton from 'components/Button/ArrowButton'
+import { Box, Flex } from '@chakra-ui/react'
+import ComponentWrapper from 'components/Wrapper/ComponentWrapper'
 import OrdersCard from 'components/Cards/OrdersCard'
 import useComponent from 'context/component'
 import FarmsCard from 'components/Cards/FarmsCard'
@@ -11,55 +10,22 @@ const FarmOrderSection = ({ farms, orders, handleClick, onOpen }) => {
   const { state, setState } = useComponent()
 
   return (
-    <Box p={20}>
-      <Box mb={10}>
-        <Heading as='h4' fontSize={{ md: '2xl' }} mb={6}>
-          Here’s how your farm(s) are doing
-        </Heading>
-        <Grid templateColumns={{ md: '70% 20%' }} gap={{ md: '10%' }}>
-          <Flex
-            align='center'
-            borderBottomWidth={1}
-            borderBottomColor='gray.200'
-          >
-            <Box
-              color={state === 'farms' ? 'cf.400' : 'gray.700'}
-              onClick={() => setState('farms')}
-              fontWeight={state === 'farms' ? 'bold' : 'normal'}
-              cursor='pointer'
-              borderBottomWidth={state === 'farms' && 2}
-              borderBottomColor='cf.400'
-              pb={3}
-            >
-              Current farms
-            </Box>
-            <Box mx={10} />
-            <Box
-              color={state === 'orders' ? 'cf.400' : 'gray.700'}
-              onClick={() => setState('orders')}
-              fontWeight={state === 'orders' ? 'bold' : 'normal'}
-              cursor='pointer'
-              borderBottomWidth={state === 'orders' && 2}
-              borderBottomColor='cf.400'
-              pb={3}
-            >
-              Orders
-            </Box>
-          </Flex>
-
-          <Box>
-            <ArrowButton handleClick={handleClick} />
-          </Box>
-        </Grid>
-      </Box>
-
+    <ComponentWrapper
+      state={state}
+      setState={setState}
+      firstStateValue='farms'
+      secondStateValue='orders'
+      firstBoxTitle='Current Farms'
+      secondBoxTitle='Orders'
+      handleClick={handleClick}
+    >
       <Box>
         <Flex>
           {state === 'farms' && <FarmsCard data={farms} />}
           {state === 'orders' && <OrdersCard data={orders} onOpen={onOpen} />}
         </Flex>
       </Box>
-    </Box>
+    </ComponentWrapper>
   )
 }
 
