122d51a1faab635c67eff8fce917ae26bf993680
DziiOG <lildzii.wd@gmail.com>
2021-04-08 20:18:20 +0000

    farm view fixes

--- src/components/Modals/FarmContractModal.js
+++ src/components/Modals/FarmContractModal.js
@@ -6,7 +6,7 @@ import Receipt from '../Utils/Receipt'
 import ModalWrapper from './ModalWrapper'
 
 const FarmContractModal = () => {
-  const { isOpen, onClose } = useComponent()
+  const { isOpen, onClose, data } = useComponent()
   return (
     <ModalWrapper
       image={require('../../assets/images/logo.png').default}
@@ -16,11 +16,13 @@ const FarmContractModal = () => {
       onClose={onClose}
     >
       <Receipt
+        data={data}
+        type={data?.type}
         title='Farm Contract'
         description='Description'
         label="Here's the confirmation of the contract for your farm"
         text='Have an issue with your farm contract?'
-        buttonTitle='Download contract'
+        buttonTitle='Download agreement'
       />
     </ModalWrapper>
   )
--- src/components/Utils/Receipt.js
+++ src/components/Utils/Receipt.js
@@ -1,11 +1,40 @@
+/* eslint-disable react/jsx-curly-newline */
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Box, Divider, Flex, Heading, Text } from '@chakra-ui/react'
 
 import Button from 'components/Button'
+import useApi from 'context/api'
 import FarmInfo from 'components/Cards/FarmInfo'
+import { saveAs } from 'file-saver'
 
-const Receipt = ({ title, description, buttonTitle, label, text }) => {
+const Receipt = ({
+  title,
+  description,
+  buttonTitle,
+  label,
+  text,
+  data,
+  type
+}) => {
+  const { downloadFile } = useApi()
+
+  const [loading, setLoading] = React.useState(false)
+  const [error, setError] = React.useState(null)
+
+  const _downloadOrder = async query => {
+    try {
+      setLoading(true)
+      setError(null)
+      const res = await downloadFile('orders', query)
+      let blob = new Blob([res.data], { type: 'application/pdf;charset=utf-8' })
+      setLoading(false)
+      saveAs(blob, `${query.reference}-agreement.pdf`)
+    } catch (error) {
+      setError(error)
+      setLoading(false)
+    }
+  }
   return (
     <Box>
       <Divider orientation='horizontal' my={3} />
@@ -17,7 +46,14 @@ const Receipt = ({ title, description, buttonTitle, label, text }) => {
         <Divider orientation='horizontal' my={3} />
         <Text fontSize='xs'>{label}</Text>
       </Box>
-      <FarmInfo width='100%' margin={0} />
+      <FarmInfo
+        rate={data?.exchangeRate}
+        farm={data?.farm}
+        order={data?.order}
+        currency={data?.currency}
+        width='100%'
+        margin={0}
+      />
 
       <Flex align='center' justify='space-between' my={8}>
         <Text fontSize='xs'>{text}</Text>
@@ -36,7 +72,7 @@ const Receipt = ({ title, description, buttonTitle, label, text }) => {
           shadow='none'
         />
       </Flex>
-
+      {error && !loading && alert('Opps, Something went wrong')}
       <Box w={56} mx='auto' my={6}>
         <Button
           btntitle={buttonTitle}
@@ -45,6 +81,14 @@ const Receipt = ({ title, description, buttonTitle, label, text }) => {
           mx='auto'
           fontSize='md'
           rounded='30px'
+          isLoading={loading}
+          isDisabled={loading}
+          onClick={() =>
+            _downloadOrder({
+              reference: data?.farm?.order?.reference,
+              type: type
+            })
+          }
         />
       </Box>
     </Box>
@@ -55,6 +99,8 @@ Receipt.propTypes = {
   description: PropTypes.string,
   label: PropTypes.string,
   text: PropTypes.string,
+  data: PropTypes.object.isRequired,
+  type: PropTypes.string.isRequired,
   buttonTitle: PropTypes.string.isRequired
 }
 
