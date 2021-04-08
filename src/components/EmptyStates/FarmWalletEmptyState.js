760a441c7b6c5976b9c8131b655b3145f3891163
DziiOG <lildzii.wd@gmail.com>
2021-04-08 18:23:05 +0000

    farm view fixes

--- src/components/EmptyStates/FarmWalletEmptyState.js
+++ src/components/EmptyStates/FarmWalletEmptyState.js
@@ -1,32 +1,20 @@
 import React from 'react'
-import { Box, Heading, Text } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+import { getCurrentDayParting } from 'helpers/misc'
 import Greetings from 'components/Utils/Greetings'
 import useAuth from 'context/auth'
 import PropTypes from 'prop-types'
 const FarmWalletEmptyState = ({ children }) => {
   const { isAuthenticated } = useAuth()
   const { user } = isAuthenticated()
+  const { message } = getCurrentDayParting()
+
   return (
     <Box>
-      <Box bg='cf.gray.400' w='100%' h={{ md: 80 }}>
-        <Box py={{ md: 28 }} px={{ md: 20 }}>
-          <Heading
-            lineHeight='56px'
-            fontWeight='700'
-            fontSize='48px'
-            dangerouslySetInnerHTML={{
-              __html: 'Welcome'
-            }}
-          />
-          <Text
-            lineHeight='56px'
-            fontWeight='400'
-            fontSize='48px'
-            dangerouslySetInnerHTML={{ __html: `Farmer ${user?.firstName}` }}
-          />
-        </Box>
-      </Box>
-      <Greetings />
+      <Greetings
+        title={`${message} Farmer ${user?.firstName}`}
+        text={"Click on the card below to view your farm's wallet"}
+      />
       {children}
     </Box>
   )
