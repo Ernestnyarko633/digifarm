530631e042d9813af834c3f88cf5c3a6990dbfe1
DziiOG <lildzii.wd@gmail.com>
2021-04-11 04:55:36 +0000

    fading changes*

--- src/components/Dynamic/Cards/FarmReceiptCard.js
+++ src/components/Dynamic/Cards/FarmReceiptCard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import Button from 'components/Button'
 import useAuth from 'context/auth'
 import useApi from 'context/api'
+import Fade from 'react-reveal/Fade'
 import { saveAs } from 'file-saver'
 
 export default function FarmReceiptCard({ farm, title, type }) {
@@ -27,61 +28,63 @@ export default function FarmReceiptCard({ farm, title, type }) {
     }
   }
   return (
-    <Box
-      bg='white'
-      rounded='lg'
-      filter='drop-shadow(0px 2px 20px rgba(0, 0, 0, 0.1))'
-      p={4}
-    >
-      <Flex align='center' justify='space-between'>
-        <Flex align='center'>
-          <Avatar src={farm?.order?.product?.cropVariety?.imageUrl} />
-          <Box ml={2}>
-            <Text fontSize='lg' fontWeight={700}>
-              {`${user?.firstName}'s Farm ${title}`}
-            </Text>
-            <Text fontSize='xs' color='gray.500' mt={-2}>
-              {`${farm?.order?.product?.location?.state} ${farm?.order?.product?.location?.country}`}
-            </Text>
+    <Fade bottom>
+      <Box
+        bg='white'
+        rounded='lg'
+        filter='drop-shadow(0px 2px 20px rgba(0, 0, 0, 0.1))'
+        p={4}
+      >
+        <Flex align='center' justify='space-between'>
+          <Flex align='center'>
+            <Avatar src={farm?.order?.product?.cropVariety?.imageUrl} />
+            <Box ml={2}>
+              <Text fontSize='lg' fontWeight={700}>
+                {`${user?.firstName}'s Farm ${title}`}
+              </Text>
+              <Text fontSize='xs' color='gray.500' mt={-2}>
+                {`${farm?.order?.product?.location?.state} ${farm?.order?.product?.location?.country}`}
+              </Text>
+            </Box>
+          </Flex>
+
+          <Box>
+            <Button
+              btntitle={`View ${title.toLowerCase()}`}
+              onClick={() => {
+                if (farm?.order?.agreement) {
+                  return (window.location = farm?.order?.agreement)
+                }
+                return _downloadOrder({
+                  reference: farm?.order?.reference,
+                  type: type
+                })
+              }}
+              bg='white'
+              borderWidth={1}
+              borderColor='cf.400'
+              color='cf.400'
+              rounded='30px'
+              h={10}
+              width={32}
+              _hover={{ bg: 'white' }}
+              shadow='none'
+              isDisabled={loading ? true : false}
+              isLoading={loading ? true : false}
+            />
+            {error && !loading && alert('Opps, Something went wrong')}
           </Box>
         </Flex>
 
-        <Box>
-          <Button
-            btntitle={`View ${title.toLowerCase()}`}
-            onClick={() => {
-              if (farm?.order?.agreement) {
-                return (window.location = farm?.order?.agreement)
-              }
-              return _downloadOrder({
-                reference: farm?.order?.reference,
-                type: type
-              })
-            }}
-            bg='white'
-            borderWidth={1}
-            borderColor='cf.400'
-            color='cf.400'
-            rounded='30px'
-            h={10}
-            width={32}
-            _hover={{ bg: 'white' }}
-            shadow='none'
-            isDisabled={loading ? true : false}
-            isLoading={loading ? true : false}
-          />
-          {error && !loading && alert('Opps, Something went wrong')}
+        <Box mt={8}>
+          <Image src={require('../../../assets/images/document.png').default} />
+          <Text w={64} fontSize='sm' color='gray.600' mt={4}>
+            Protect your farm from natural perils like flood, fire, pests,
+            strong winds with insurance for your farm
+          </Text>
         </Box>
-      </Flex>
-
-      <Box mt={8}>
-        <Image src={require('../../../assets/images/document.png').default} />
-        <Text w={64} fontSize='sm' color='gray.600' mt={4}>
-          Protect your farm from natural perils like flood, fire, pests, strong
-          winds with insurance for your farm
-        </Text>
       </Box>
-    </Box>
+    </Fade>
   )
 }
 
--- src/components/Dynamic/Cards/WeatherCard.js
+++ src/components/Dynamic/Cards/WeatherCard.js
@@ -2,42 +2,45 @@ import { Box, Flex, Heading, Icon, Text } from '@chakra-ui/react'
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Cloud } from 'theme/Icons'
+import Fade from 'react-reveal/Fade'
 
 export default function WeatherCard({ icon, title, duration, value }) {
   return (
-    <Box
-      w='100%'
-      rounded='lg'
-      filter='drop-shadow(0px 2px 20px rgba(0, 0, 0, 0.1))'
-      p={8}
-      bg='white'
-    >
-      <Flex
-        align='center'
-        justify='space-between'
-        borderBottomWidth={1}
-        borderBottomColor='gray.200'
-        pb={3}
+    <Fade left>
+      <Box
+        w='100%'
+        rounded='lg'
+        filter='drop-shadow(0px 2px 20px rgba(0, 0, 0, 0.1))'
+        p={8}
+        bg='white'
       >
-        <Text fontWeight={900}>
-          <Icon as={icon} mr={1} />
-          {title}
-        </Text>
-        <Text color='gray.500' fontSize='sm'>
-          {duration}
-        </Text>
-      </Flex>
+        <Flex
+          align='center'
+          justify='space-between'
+          borderBottomWidth={1}
+          borderBottomColor='gray.200'
+          pb={3}
+        >
+          <Text fontWeight={900}>
+            <Icon as={icon} mr={1} />
+            {title}
+          </Text>
+          <Text color='gray.500' fontSize='sm'>
+            {duration}
+          </Text>
+        </Flex>
 
-      <Box mt={2} w={{ md: 70 }}>
-        <Icon as={Cloud} boxSize={10} />
-        <Heading fontSize={{ md: '6xl' }} fontWeight={900} mt={1}>
-          {value?.toFixed(2)} C
-        </Heading>
-        <Text fontSize='sm' color='gray.500' mt={2}>
-          Weather is cloudy today. Looks like it might be raining on your farm
-        </Text>
+        <Box mt={2} w={{ md: 70 }}>
+          <Icon as={Cloud} boxSize={10} />
+          <Heading fontSize={{ md: '6xl' }} fontWeight={900} mt={1}>
+            {value?.toFixed(2)} C
+          </Heading>
+          <Text fontSize='sm' color='gray.500' mt={2}>
+            Weather is cloudy today. Looks like it might be raining on your farm
+          </Text>
+        </Box>
       </Box>
-    </Box>
+    </Fade>
   )
 }
 
