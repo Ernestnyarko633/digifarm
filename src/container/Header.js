049eca883b7fd0a974b2861e3c8f1e1aa4e00966
Chinedu Ekene Okpala <allstackdev@gmail.com>
2021-04-21 15:15:41 +0000

    merge dev

--- src/container/Header.js
+++ src/container/Header.js
@@ -1,34 +1,33 @@
-/* eslint-disable */
-import React from 'react';
-import { Avatar, Box, Flex, Icon, Link, Text } from '@chakra-ui/react';
-import { motion, AnimatePresence } from 'framer-motion';
-import { Menu } from '@headlessui/react';
-import { FiChevronDown, FiChevronUp, FiUser } from 'react-icons/fi';
+import React from 'react'
+import { Avatar, Box, Flex, Icon, Image, Link, Text } from '@chakra-ui/react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { Menu } from '@headlessui/react'
+import { FiChevronDown, FiChevronUp, FiUser } from 'react-icons/fi'
 // import { BiCog, BiSupport, BiHistory } from 'react-icons/bi'
-import { HiOutlineLogout } from 'react-icons/hi';
-import { BsBell } from 'react-icons/bs';
-import { Link as ReachRouter } from 'react-router-dom';
+import { HiOutlineLogout } from 'react-icons/hi'
+import { BsBell } from 'react-icons/bs'
+import { Link as ReachRouter } from 'react-router-dom'
 
-import Logo1 from 'assets/images/logo@1x.svg';
-import Logo2 from 'assets/images/logo@2x.svg';
-import Logo3 from 'assets/images/logo@3x.svg';
+import Logo1 from 'assets/images/logo@1x.svg'
+import Logo2 from 'assets/images/logo@2x.svg'
+import Logo3 from 'assets/images/logo@3x.svg'
 
-import useAuth from 'context/auth';
+import useAuth from 'context/auth'
 
 const menuLinks = [
   { name: 'Profile', icon: FiUser, link: '/profile' },
   // { name: 'History', icon: BiHistory, link: '/history' },
   // { name: 'Settings', icon: BiCog, link: '/settings' },
   // { name: 'Help Center', icon: BiSupport, link: '/help' }
-  { name: 'Logout', icon: HiOutlineLogout, link: '/logout' },
-];
+  { name: 'Logout', icon: HiOutlineLogout, link: '/logout' }
+]
 
-const MotionBox = motion.custom(Box);
+const MotionBox = motion.custom(Box)
 
 const Header = () => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated } = useAuth()
 
-  const { user } = isAuthenticated();
+  const { user } = isAuthenticated()
 
   return (
     <Flex
@@ -47,22 +46,14 @@ const Header = () => {
       px={{ base: 4, md: 36 }}
       overflowX={{ base: 'hidden', md: 'visible' }}
     >
-      <ReachRouter to='/dashboard'>
-        <Link _hover={{ textDecor: 'none' }}>
-          <Box
-            w={{ base: '84.47px', md: '113px', xl: '169px' }}
-            h={{ base: 6, md: 8, xl: 16 }}
-            bgImage={{
-              base: `url('${Logo1}')`,
-              md: `url('${Logo2}')`,
-              xl: `url('${Logo3}')`,
-            }}
-            bgSize='contain'
-            bgPos='center'
-            bgRepeat='no-repeat'
+      <Link as={ReachRouter} to='/dashboard' _hover={{ textDecor: 'none' }}>
+        <Box as='picture'>
+          <Image
+            src={Logo1}
+            srcSet={`${Logo1} 300w, ${Logo2} 768w, ${Logo3} 1280w`}
           />
-        </Link>
-      </ReachRouter>
+        </Box>
+      </Link>
 
       <Flex align='center'>
         <Flex align='center' mr={{ base: 4, md: 10 }}>
@@ -100,7 +91,7 @@ const Header = () => {
                     animate={{
                       opacity: 1,
                       y: 0,
-                      transition: { duration: 0.3 },
+                      transition: { duration: 0.3 }
                     }}
                     exit={{ opacity: 0, y: 50 }}
                     pos={{ base: 'fixed', md: 'absolute' }}
@@ -121,20 +112,20 @@ const Header = () => {
                           as={MotionBox}
                           custom={i}
                           variants={{
-                            hidden: (i) => ({
+                            hidden: i => ({
                               y: -50 * i,
-                              opacity: 0,
+                              opacity: 0
                             }),
-                            visible: (i) => ({
+                            visible: i => ({
                               y: 0,
                               opacity: 1,
                               transition: {
-                                delay: i * 0.025,
-                              },
+                                delay: i * 0.025
+                              }
                             }),
                             removed: {
-                              y: 30 * i,
-                            },
+                              y: 30 * i
+                            }
                           }}
                           initial='hidden'
                           animate='visible'
@@ -145,7 +136,7 @@ const Header = () => {
                               py={2}
                               px={6}
                               _hover={{
-                                textDecor: 'none',
+                                textDecor: 'none'
                               }}
                               bg={active && 'cf.400'}
                               color={active && 'white'}
@@ -167,7 +158,7 @@ const Header = () => {
         </Menu>
       </Flex>
     </Flex>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
