0677023ab515a1d41063cb2609e0b59fb69864d7
nanayaw <gabsco208309@hotmail.com>
2021-05-11 11:16:33 +0000

    fixed issues with Docker node version and other fixes

--- src/components/Cards/FarmsCard.js
+++ src/components/Cards/FarmsCard.js
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 import { Flex } from '@chakra-ui/react'
 import FarmCard from './FarmCard'
 
-const MotionFlex = motion.custom(Flex)
+const MotionFlex = motion(Flex)
 const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] }
 
 const FarmsCard = ({ data, currentSlide }) => {
