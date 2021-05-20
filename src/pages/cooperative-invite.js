52aee27997c987a44395895833243335c91e206f
Chinedu Ekene Okpala <allstackdev@gmail.com>
2021-05-20 08:34:27 +0000

    update cooperative-invite page

--- src/pages/cooperative-invite.js
+++ src/pages/cooperative-invite.js
@@ -1,80 +1,73 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import QueryString from 'query-string'
+import { decode } from 'js-base64'
+import { Box } from '@chakra-ui/react'
 
+import { replaceURI } from 'helpers/misc'
 import FetchCard from 'components/FetchCard'
-import useAuth from 'context/auth'
 import useApi from 'context/api'
-import { Box } from '@chakra-ui/layout'
 
-const CooperativeInvite = ({
-    history: { replace },
-    match: { params },
-    location: { search }
-}) => {
-    document.title = 'Cooperative invite...'
-    const [isLoading, setIsLoading] = useState(false)
-    const [reload, setReload] = useState(0)
-    const [error, setError] = useState(false)
+const CooperativeInvite = ({ history: { replace }, match: { params } }) => {
+  document.title = 'Cooperative invite...'
+  const [isLoading, setIsLoading] = useState(0)
+  const [reload, setReload] = useState(0)
+  const [error, setError] = useState(false)
 
-  const { token } = params
+  const { acceptInvite } = useApi()
+  const { email, _id } = decode(params.token)
 
   const triggerReload = () => setReload(prevState => prevState + 1)
 
-
   useEffect(() => {
     let mounted = true
-    if (mounted) {
-        // Check to see if a token exist then use token to fetch user data else return user to auth service app
-        if (token) {
-            try {
-                setIsLoading(true)
-                // fetch user data
-                const { data: user } = await getUser()
-  
-                // store user data
-                store({ user })
-  
-                setTimeout(() => {
-                  replace(JSON.parse(to || null) || '/dashboard')
-                }, 1000)
-              } catch (error) {
-                if (error) {
-                  if ([401, 403].includes(error.status)) {
-                    replaceURI('AUTH', '/redirects?from=DIGITAL_FARMER&off=true')
-                  } else {
-                    setError(error.message)
-                  }
-                } else {
-                  setError('Unexpected network error')
-                }
-              } finally {
-                setIsLoading(false)
-              }
+    const runAcceptInvite = async () => {
+      try {
+        setIsLoading(true)
+        const res = await acceptInvite({ email, _id })
+        // setTimeout(() => {
+        //   replace(JSON.parse(to || null) || '/dashboard')
+        // }, 1000)
+      } catch (error) {
+        if (error) {
+          if ([401, 403].includes(error.status)) {
+            replaceURI('AUTH', '/redirects?from=DIGITAL_FARMER&off=true')
+          } else {
+            setError(error.message)
+          }
         } else {
-          replaceURI('AUTH', '/redirects?from=DIGITAL_FARMER&off=false')
+          setError('Unexpected network error')
         }
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    if (mounted && email && _id) {
+      runAcceptInvite()
     }
     return () => (mounted = false)
-  }, [store, getUser, isAuthenticated, replace, token, to, reload])
+  }, [acceptInvite, replace, reload, email, _id])
 
-
-    return isLoading || error ? (
+  return (
+    <Box>
+      {isLoading || error ? (
         <FetchCard
-        direction='column'
-        align='center'
-        justify='center'
-        reload={triggerReload}
-        loading={isLoading}
-        error={error}
-      />
-    ): (
-        <Box></Box>
-    )
+          direction='column'
+          align='center'
+          justify='center'
+          reload={triggerReload}
+          loading={isLoading}
+          error={error}
+        />
+      ) : (
+        <Box>{/* Add ui for */}</Box>
+      )}
+    </Box>
+  )
 }
 
 CooperativeInvite.propTypes = {
-
+  match: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
 }
 
 export default CooperativeInvite
