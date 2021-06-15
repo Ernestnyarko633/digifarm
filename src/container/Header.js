a1fbd3f58f27ab870230c3cf7e7b7952d539be56
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@hotmail.com>
2021-06-15 14:29:46 +0000

    notifications updated and created a reusable component for notification item

--- src/container/Header.js
+++ src/container/Header.js
@@ -1,54 +1,57 @@
-import React from 'react'
-import { Box, Flex, Image, Link } from '@chakra-ui/react'
-import { Link as ReachRouter } from 'react-router-dom'
-import { useQuery, useMutation, useQueryClient } from 'react-query'
+/* eslint-disable */
+import React from "react";
+import { Box, Flex, Image, Link } from "@chakra-ui/react";
+import { Link as ReachRouter } from "react-router-dom";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 
-import Logo1 from 'assets/images/logo@1x.svg'
-import Logo2 from 'assets/images/logo@2x.svg'
-import Logo3 from 'assets/images/logo@3x.svg'
+import Logo1 from "assets/images/logo@1x.svg";
+import Logo2 from "assets/images/logo@2x.svg";
+import Logo3 from "assets/images/logo@3x.svg";
 
-import useAuth from 'context/auth'
-import useApi from 'context/api'
-import Notifications from 'components/Utils/Notifications'
-import Profile from 'components/Utils/Profile'
+import useAuth from "context/auth";
+import useApi from "context/api";
+import Notifications from "components/Utils/Notifications";
+import Profile from "components/Utils/Profile";
 
 const Header = () => {
-  const { isAuthenticated } = useAuth()
-  const { user } = isAuthenticated()
-  const { getNotifications, updateNotification } = useApi()
+  const { isAuthenticated } = useAuth();
+  const { user } = isAuthenticated();
+  const { getNotifications, updateNotification } = useApi();
 
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
-  const { isLoading, data } = useQuery('notifications', () =>
+  const { isLoading, data } = useQuery("notifications", () =>
     getNotifications({
-      accessLevel: 'DIGITAL_FARMER',
-      userId: user._id
+      accessLevel: "DIGITAL_FARMER",
+      userId: user._id,
     })
-  )
+  );
 
-  const mutation = useMutation(id => updateNotification(id, user._id), {
-    onSuccess: () => queryClient.invalidateQueries('notifications')
-  })
+  console.log("data", data);
+
+  const mutation = useMutation((id) => updateNotification(id, user._id), {
+    onSuccess: () => queryClient.invalidateQueries("notifications"),
+  });
 
   return (
     <Flex
       top={0}
-      w='100%'
-      as='header'
-      pos='fixed'
+      w="100%"
+      as="header"
+      pos="fixed"
       zIndex={100}
-      align='center'
-      bgColor='white'
+      align="center"
+      bgColor="white"
       h={{ base: 14, md: 16 }}
-      gridArea='header'
-      justify='space-between'
+      gridArea="header"
+      justify="space-between"
       borderBottomWidth={1}
-      borderBottomColor='gray.300'
+      borderBottomColor="gray.300"
       px={{ base: 4, xl: 20 }}
-      overflowX={{ base: 'hidden', md: 'visible' }}
+      overflowX={{ base: "hidden", md: "visible" }}
     >
-      <Link as={ReachRouter} to='/dashboard' _hover={{ textDecor: 'none' }}>
-        <Box as='picture'>
+      <Link as={ReachRouter} to="/dashboard" _hover={{ textDecor: "none" }}>
+        <Box as="picture">
           <Image
             h={{ base: 8, md: 10 }}
             src={Logo1}
@@ -57,7 +60,7 @@ const Header = () => {
         </Box>
       </Link>
 
-      <Flex align='center'>
+      <Flex align="center">
         <Notifications
           notifications={data?.notifications}
           loading={isLoading}
@@ -67,7 +70,7 @@ const Header = () => {
         <Profile user={user} />
       </Flex>
     </Flex>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
