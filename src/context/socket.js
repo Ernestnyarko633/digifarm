7be6d76aced7f55315acc4bb37df937a04f26096
Titan <lildzii.wd@gmail.com>
2021-09-10 16:51:56 +0000

    update

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,17 +1,16 @@
 /* eslint-disable no-console */
 import React, { createContext, useContext } from 'react'
 import PropTypes from 'prop-types'
-//import useWebSocket from 'hooks/useWebSocket'
+import getConfigs from 'utils/configs'
+import useWebSocket from 'hooks/useWebSocket'
 const SocketContext = createContext()
 
 export const SocketContextProvider = ({ children }) => {
-  //const type = 'DIGITAL_FARMER'
-  // useWebSocket(
-  //   'https://apis-notifications-test.completefarmer.com',
-  //   null,
-  //   type,
-  //   d => console.log('hello', d)
-  // )
+  const { SOCKET_NOTIFICATION_API } = getConfigs()
+  const type = 'DIGITAL_FARMER'
+  useWebSocket(SOCKET_NOTIFICATION_API, null, type, d =>
+    console.log('hello', d)
+  )
 
   return <SocketContext.Provider value={{}}>{children}</SocketContext.Provider>
 }
