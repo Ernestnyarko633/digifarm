ec05137e526b2b626033d366a386f40580e7c4ae
Titan <lildzii.wd@gmail.com>
2021-09-17 12:19:24 +0000

    updated digital farmer payment

--- src/context/start-farm.js
+++ src/context/start-farm.js
@@ -15,7 +15,7 @@ import useRollover from './rollover'
 import useComponent from './component'
 
 const dcc = Constants.countries.find(c => c.id === 'US')
-const dpo = Constants.paymentOptions[2]
+const dpo = Constants.paymentOptions[0]
 
 const StartFarmContext = createContext({})
 
@@ -335,6 +335,121 @@ export const StartFarmContextProvider = ({ children }) => {
       setSubmitting(false)
     }
   }
+  const handleTazapayPayment = async (user, store, order, product) => {
+    try {
+      setSubmitting(true)
+      //check if user has account
+      if (!user?.escrowId) {
+        const payload = {
+          email: user.email, // user email
+          first_name: user.firstName, // user firstname
+          last_name: user.lastName, // user lastName
+          country: getCode(user.address.country).toUpperCase(), // get the country of the human being and get the ISO code for it transform it to Upper Case letters
+          ind_bus_type: 'Individual' // Individual
+        }
+
+        const response = await createEscrowAccount(payload) // create escrow account for user or client
+
+        // if successful
+        if (response.data) {
+          // patch the user with new information account id
+          const res = await patchUser(user?._id, {
+            escrowId: response.data.account_id
+          })
+
+          // hopefully it should be successful unless someone did something behind the scence in that case we should have a successful update
+          if (res.data) {
+            store({ user: res.data })
+          } else {
+            // else throw this unable to patch
+            throw new Error('Unable to update user account details')
+          }
+        } else {
+          // else throw this if unable to create account
+          throw new Error('Unable to process/create escrow account')
+        }
+      }
+
+      // initiate escrow payment
+
+      const escrow_payload = {
+        initiated_by: user.escrowId, // escrow account of user who started this whole mess
+        seller_id: ESCROW_SELLER_ID, // escrow account of the person selling
+        buyer_id: user.escrowId, // escrow account of the person buying
+        order_id: order._id, // order id of the farm
+        purpose: 'FARM_PURCHASE', // type
+        txn_description: `Purchase of ${product.name} farm`, // description of transaction
+        invoice_amount: order.cost, // cost of transaction
+        invoice_currency: 'USD', // currency
+
+        // must always be false
+        is_milestone: false, // false
+
+        // this two information might change
+        fee_paid_by: 'buyer', // buyer
+        fee_percentage: 100 // 100
+      }
+
+      // if everything is okay send the payload
+      const create_escrow_response = await createEscrow(escrow_payload)
+
+      // if successful
+      if (create_escrow_response.data) {
+        const payload = {
+          txn_no: create_escrow_response.data.txn_no,
+          complete_url: `${window.location.origin}/tazapay?order=${order._id}&txn_no=${create_escrow_response.data.txn_no}`,
+          error_url: `${window.location.origin}/tazapay?order=${order._id}&txn_no=${create_escrow_response.data.txn_no}&error=true`
+        }
+
+        const response = await payEscrow(payload)
+        window.onbeforeunload = null
+
+        if (response.data.redirect_url) {
+          const res = await patchOrder(order._id, {
+            redirect: response.data.redirect_url
+          })
+          if (res.data) {
+            window.onbeforeunload = null
+            window.location.href = response.data.redirect_url
+          }
+        } else {
+          throw new Error('Unexpected payment gateway failure')
+        }
+      } else {
+        // else throw this error if unsuccesful in creating escrow account
+        throw new Error('Unable to process request')
+      }
+    } catch (error) {
+      if (error) {
+        if ([401, 403].includes(error.status)) {
+          setSession(false)
+        } else {
+          toast({
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+            position: 'top-right',
+            title: 'An error occurred.',
+            description:
+              (error?.data?.message ||
+                error?.message ||
+                'Unknown error occurred') + '.'
+          })
+        }
+      } else {
+        toast({
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+          position: 'top-right',
+          title: 'An error occurred.',
+          description: 'Unexpected network error.'
+        })
+      }
+    } finally {
+      setSubmitting(false)
+    }
+  }
 
   const handleCreateCooperative = async cooperativeTypeId => {
     try {
@@ -402,6 +517,7 @@ export const StartFarmContextProvider = ({ children }) => {
       if (paymentOption === Constants.paymentOptions[0]) {
         const q = 'USD_GHS'
         const res = await getExchangeRate({ q })
+
         if (!res.data) {
           throw new Error('Unknown error occurred, try again')
         }
@@ -417,90 +533,13 @@ export const StartFarmContextProvider = ({ children }) => {
           throw new Error('Unexpected payment gateway failure')
         }
         window.location.href = result.data.authorization_url
-      } else if (paymentOption === Constants.paymentOptions[2]) {
-        //check if user has account
-        if (!user?.escrow_account_id) {
-          const payload = {
-            email: user.email, // user email
-            first_name: user.firstName, // user firstname
-            last_name: user.lastName, // user lastName
-            country: getCode(user.address.country).toUpperCase(), // get the country of the human being and get the ISO code for it transform it to Upper Case letters
-            ind_bus_type: 'Individual' // Individual
-          }
-
-          const response = await createEscrowAccount(payload) // create escrow account for user or client
-
-          // if successful
-          if (response.data) {
-            // patch the user with new information account id
-            const res = await patchUser(user?._id, {
-              escrow_account_id: response.data.account_id
-            })
-
-            // hopefully it should be successful unless someone did something behind the scence in that case we should have a successful update
-            if (res.data) {
-              store({ user: res.data })
-            } else {
-              // else throw this unable to patch
-              throw new Error('Unable to update user account details')
-            }
-          } else {
-            // else throw this if unable to create account
-            throw new Error('Unable to process/create escrow account')
-          }
-        }
-
-        // initiate escrow payment
-
-        const escrow_payload = {
-          initiated_by: user.escrow_account_id, // escrow account of user who started this whole mess
-          seller_id: ESCROW_SELLER_ID, // escrow account of the person selling
-          buyer_id: user.escrow_account_id, // escrow account of the person buying
-          order_id: id || order._id, // order id of the farm
-          purpose: 'FARM_PURCHASE', // type
-          txn_description: `Purchase of ${selectedFarm.name} farm`, // description of transaction
-          invoice_amount: cost || order.cost, // cost of transaction
-          invoice_currency: 'USD', // currency
-
-          // must always be false
-          is_milestone: false, // false
-
-          // this two information might change
-          fee_paid_by: 'buyer', // buyer
-          fee_percentage: 100 // 100
-        }
-
-        // if everything is okay send the payload
-        const create_escrow_response = await createEscrow(escrow_payload)
-
-        // if successful
-        if (create_escrow_response.data) {
-          const payload = {
-            txn_no: create_escrow_response.data.txn_no,
-            complete_url: `${window.location.origin}/tazapay?order=${
-              id || order._id
-            }&txn_no=${create_escrow_response.data.txn_no}`,
-            error_url: `${window.location.origin}/tazapay?order=${
-              id || order._id
-            }&txn_no=${create_escrow_response.data.txn_no}&error=true`
-          }
-
-          const response = await payEscrow(payload)
-          window.onbeforeunload = null
-
-          if (response.data.redirect_url) {
-            await patchOrder(id || order._id, {
-              redirect: response.data.redirect_url
-            })
-            window.onbeforeunload = null
-            window.location.href = response.data.redirect_url
-          } else {
-            throw new Error('Unexpected payment gateway failure')
-          }
-        } else {
-          // else throw this error if unsuccesful in creating escrow account
-          throw new Error('Unable to process request')
-        }
+      } else if (paymentOption === Constants.paymentOptions[1]) {
+        await handleTazapayPayment(
+          user,
+          store,
+          order || { _id: id },
+          order.product
+        )
       } else {
         const res = await initiatePayment(data)
         await patchOrder(res?.data?.order_id?.$oid, {
@@ -609,6 +648,7 @@ export const StartFarmContextProvider = ({ children }) => {
         handleCreateOrder,
         setCooperativeName,
         setCoopConfigErrors,
+        handleTazapayPayment,
         handleCreateCooperative,
         selectedCooperativeType,
         setSelectedCooperativeType,
