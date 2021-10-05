e4977becd2d69daf45806b3f07c7e2b8125e3396
Nutifafa Kpodo <nutifafafagan@gmail.com>
2021-10-05 18:00:03 +0000

    DI-54- Rendering of farm manger updates on right side bar.

--- src/container/RightSidebar.js
+++ src/container/RightSidebar.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Box, Grid, Heading } from '@chakra-ui/react'
@@ -8,9 +9,14 @@ import useApi from 'context/api'
 import useFetch from 'hooks/useFetch'
 import FetchCard from 'components/FetchCard'
 import CooperativesCard from 'components/Cards/CooperativesCard'
+import FarmManagerUpdateCard from 'components/Cards/FarmManagerUpdateCard'
+import { useFarmData } from 'hooks/useFarmData'
 
 const RightSidebar = ({ onOpen, setSelectedData }) => {
-  const [doc, setDocData] = React.useState(null)
+  const [announcements, setAnnouncementsData] = React.useState(null)
+  const [managerUpdates, setManagerUpdatesData] = React.useState(null)
+  const [errors, setErrors] = React.useState(null)
+
   const [reload, setReload] = React.useState(0)
   const [loading, setLoading] = React.useState(false)
   const { getCooperatives } = useApi()
@@ -25,24 +31,64 @@ const RightSidebar = ({ onOpen, setSelectedData }) => {
     accessToken: PRISMIC_ACCESS_TOKEN
   })
 
+  const { myFarms } = useFarmData()
+  // eslint-disable-next-line no-console
+  console.log('farms', myFarms)
+
   React.useEffect(() => {
     let mounted = true
-    if (mounted && !doc) {
+    if (mounted && (!announcements || !managerUpdates)) {
       const fetchData = async () => {
-        setLoading(true)
-        const response = await Client.query(
-          Prismic.Predicates.at('document.type', 'announcements')
-        )
-        if (response) {
-          setDocData(response.results)
-        }
+        try {
+          setLoading(true)
+          // await news and video prismic documents
+          const [announcements_response, managerUpdates_response] =
+            await Promise.all([
+              Client.query(
+                Prismic.Predicates.at('document.type', 'announcements'),
+                {
+                  pageSize: 200
+                }
+              ),
+              Client.query(
+                Prismic.Predicates.at('document.type', 'manager_update'),
+                { pageSize: 200 }
+              )
+            ])
 
-        setLoading(false)
+          // if response exists set the corresponding data
+          if (announcements_response)
+            setAnnouncementsData(announcements_response?.results || [])
+          if (managerUpdates_response)
+            setManagerUpdatesData(managerUpdates_response?.results || [])
+        } catch (err) {
+          // catch errors if any and st them
+          setErrors(err?.message || err || 'Could not fetch data')
+        } finally {
+          //finally set loading too false
+          setLoading(false)
+        }
       }
       fetchData()
     }
     return () => (mounted = false)
-  }, [Client, doc])
+  }, [Client, announcements, managerUpdates])
+
+  // eslint-disable-next-line no-console
+  console.log(announcements, 'announcements')
+  // eslint-disable-next-line no-console
+  console.log(managerUpdates, 'managerUpdates')
+
+  // const farmUpdates = managerUpdates?.filter(
+  //   ({ update }) =>
+  //     !myFarms?.some(
+  //       ({ farm }) =>
+  //         farm?.order?.product?._id === update?.data?.farm_id[0]?.text
+  //     )
+  // )
+
+  // // eslint-disable-next-line no-console
+  // console.log('farmUpdates', farmUpdates)
 
   return (
     <Box
@@ -57,10 +103,52 @@ const RightSidebar = ({ onOpen, setSelectedData }) => {
       pb={{ base: 24, md: 0 }}
       w={{ md: '22%', xl: '22%' }}
     >
+      {loading ? (
+        <FetchCard
+          direction='column'
+          align='center'
+          justify='center'
+          mx='auto'
+          w={{ xl: 24 }}
+          h={{ xl: 80 }}
+          reload={null}
+          loading={loading}
+          error={null}
+          text='Loading farm manager`s updates'
+        />
+      ) : (
+        <>
+          {managerUpdates ? (
+            <>
+              <Heading
+                as='h4'
+                textTransform='uppercase'
+                fontSize={{ base: 'lg', md: '1xl' }}
+                fontWeight={700}
+                borderBottomWidth={1}
+                borderBottomColor='gray.300'
+                pb={2}
+              >
+                Manager's Update
+              </Heading>
+              <Box
+                overflowY='scroll'
+                px={2}
+                my={2}
+                h={{ base: '20%', xl: '30%' }}
+              >
+                {managerUpdates?.map((update, i) => (
+                  <FarmManagerUpdateCard key={mapKey(i)} update={update} />
+                ))}
+              </Box>
+            </>
+          ) : null}
+        </>
+      )}
       <Heading
         as='h4'
         textTransform='uppercase'
-        fontSize={{ base: 'lg', md: '2xl' }}
+        fontSize={{ base: 'lg', md: '1xl' }}
         fontWeight={700}
         borderBottomWidth={1}
         borderBottomColor='gray.300'
@@ -68,8 +156,7 @@ const RightSidebar = ({ onOpen, setSelectedData }) => {
       >
         Events
       </Heading>
-
-      <Box h='45%' px={2} my={2}>
+      <Box h='25%' px={2} my={2}>
         {loading ? (
           <FetchCard
             direction='column'
@@ -86,7 +173,7 @@ const RightSidebar = ({ onOpen, setSelectedData }) => {
         ) : (
           <>
             <Grid gap={4} mt={4} mb={4}>
-              {doc?.map((event, i) => (
+              {announcements?.map((event, i) => (
                 <EventCard
                   key={mapKey(i)}
                   onOpen={onOpen}
@@ -96,7 +183,7 @@ const RightSidebar = ({ onOpen, setSelectedData }) => {
               ))}
             </Grid>
 
-            <Grid gap={4} mt={4} mb={4} d={{ base: 'grid', md: 'none' }}>
+            {/* <Grid gap={4} mt={4} mb={4} d={{ base: 'grid', md: 'none' }}>
               {doc?.map((event, i) => (
                 <EventCard
                   key={mapKey(i)}
@@ -105,7 +192,7 @@ const RightSidebar = ({ onOpen, setSelectedData }) => {
                   event={event}
                 />
               ))}
-            </Grid>
+            </Grid> */}
           </>
         )}
       </Box>
@@ -128,8 +215,9 @@ const RightSidebar = ({ onOpen, setSelectedData }) => {
           <>
             <Heading
               as='h4'
-              fontSize={{ base: 'lg', md: '2xl' }}
+              fontSize={{ base: 'lg', md: '1xl' }}
               fontWeight={700}
+              textTransform='uppercase'
               borderBottomWidth={1}
               borderBottomColor='gray.300'
               pb={2}
@@ -142,15 +230,7 @@ const RightSidebar = ({ onOpen, setSelectedData }) => {
               h={{ base: '20%', xl: '40%' }}
               px={2}
             >
-              <Box d={{ base: 'none', md: 'block' }}>
-                <Box>
-                  {data.map(coop => (
-                    <CooperativesCard coop={coop} key={coop._id} />
-                  ))}
-                </Box>
-              </Box>
-
-              <Box d={{ base: 'block', md: 'none' }} overflowY='scroll'>
+              <Box d={{ base: 'block', md: 'block' }}>
                 {data.map(coop => (
                   <CooperativesCard coop={coop} key={coop._id} />
                 ))}
