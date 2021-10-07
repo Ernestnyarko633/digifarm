3a244e563db507b7d6af5c5418faae7aeca39b79
Nutifafa Kpodo <nutifafafagan@gmail.com>
2021-10-07 18:39:11 +0000

    DI-54-Console.log removal.

--- src/components/Dynamic/Cards/WeatherCards.js
+++ src/components/Dynamic/Cards/WeatherCards.js
@@ -12,8 +12,6 @@ const WeatherCard = ({
   farmFeedsHasError,
   reloads
 }) => {
-  // eslint-disable-next-line no-console
-  console.log('farmfeeds', farmfeeds)
   return (
     <Grid templateColumns={{ md: 'repeat(2, 1fr)' }} gap={8} my={{ md: 8 }}>
       {farmFeedsIsLoading || farmFeedsHasError ? (
